22df4560f44f3097c69a2b48542a7b955400d7fd
Samuel Sacco <SamLSacco@aol.com>
2021-05-19 14:10:31 -0400

    setup tables

---
+++ AA_questions/import_db.sql
@@ -0,0 +1,26 @@
+CREATE TABLE users (
+    id INTEGER PRIMARY KEY,
+    fname TEXT NOT NULL,
+    lname TEXT NOT NULL
+);
+
+CREATE TABLE questions (
+    id INTEGER PRIMARY KEY,
+    title TEXT NOT NULL,
+    body TEXT NOT NULL,
+    author_id INTEGER NOT NULL
+
+    FOREIGN KEY (author_id) REFERENCES users(id)
+);
+
+CREATE TABLE question_follows (
+    id INTEGER PRIMARY KEY,
+    question_id INTEGER NOT NULL,
+    user_id INTEGER NOT NULL
+
+    FOREIGN KEY (question_id) REFERENCES questions(id)
+    FOREIGN KEY (user_id) REFERENCES users(id)
+)
+
+CREATE TABLE replies (
+)
